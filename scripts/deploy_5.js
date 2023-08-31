fec6bbf05b1c8d76de03476598504e012323fddd
AbdulRehman9012 <abdulrahmanidrees90@gmail.com>
2023-08-31 22:26:21 +0500

    modified:   package-lock.json 	modified:   package.json 	renamed:    scripts/deploy.js -> scripts/deploy_1.js 	new file:   scripts/deploy_2.js 	new file:   scripts/deploy_3.js 	new file:   scripts/deploy_4.js 	new file:   scripts/deploy_5.js 	renamed:    scripts/win.js -> scripts/win_1.js 	new file:   scripts/win_2.js 	new file:   scripts/win_3.js 	new file:   scripts/win_4.js 	new file:   scripts/win_5.js

---
+++ scripts/deploy_5.js
@@ -0,0 +1,16 @@
+// replace the name of the contract with which one you want to deploy!
+const contractName = "Game5";
+
+async function main() {
+  const Game = await hre.ethers.getContractFactory(contractName);
+  // if you need to add constructor arguments for the particular game, add them here:
+  const game = await Game.deploy();
+  console.log(`${contractName} deployed to address: ${game.address}`);
+}
+
+main()
+ .then(() => process.exit(0))
+ .catch(error => {
+   console.error(error);
+   process.exit(1);
+ });
