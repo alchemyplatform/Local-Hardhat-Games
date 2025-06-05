9163dfd59edd10f4827e573eed88d1d527489942
kaleabm18 <kaleabm18@gmail.com>
2025-06-05 14:47:33 +0300

    updated some script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 // replace the name of the contract with which one you want to deploy!
-const contractName = "Game1";
+const contractName = "Game2";
 
 async function main() {
   const Game = await hre.ethers.getContractFactory(contractName);
--- scripts/win.js
+++ scripts/win.js
@@ -1,18 +1,22 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+const contractName = "Game2";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
-    const tx = await game.win();
+    // const tx = await game.win();
+
+    const tx1 = await game.setX(43);
+    const receipt1 = await tx1.wait();
+    console.log(receipt1);
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
-    const receipt = await tx.wait();
-    console.log(receipt);
+    // const receipt = await tx.wait();
+    // console.log(receipt);
 }
 
 main()
